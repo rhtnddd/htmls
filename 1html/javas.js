778adc9b8d312247a96f1d098f1495d69dc389b6
성민규 <sistsus0917@gmail.com>
2025-06-01 01:16:45 +0900

    도끼 다만듬

--- 1html/javas.js
+++ 1html/javas.js
@@ -13,7 +13,7 @@ let audio1 = new Audio('exploration-chiptune-rpg-adventure-theme-336428.mp3');
 audio.volume = 0.3;
 let myHeading = document.getElementById("myHeading"); //bgm만듬
 let upmoney = document.getElementById("upmoney");
-upmoney.textContent = " 테무산 도끼:"+upgrade+"원";
+upmoney.textContent = " 다이소 도끼:"+upgrade+"골드";
 myHeading.textContent = "머니머니머니:"+money;
 function moveSlide() {
   track.style.transform = `translateX(-${150 * currentIndex}px)`;
@@ -26,53 +26,179 @@ bgm.onclick=()=>{
 up.onclick=()=>{
   if(money>=upgrade)
   {
-    if(okclick==0)
+    if(okclick===0)
     {
+      currentIndex=2;
       money-=upgrade;
       okclick=1;
       upgrade*=3;
       myHeading.textContent = "머니머니머니:"+money;
-      upmoney.textContent = "다이소 도끼:"+upgrade+"원";
+      upmoney.textContent = "고물점에서 산 도끼:"+upgrade+"골드";
       moneyup+=1;
     }
-    else if(okclick==1)
+    else if(okclick===1)
     {
+      currentIndex=4;
       money-=upgrade;
       okclick=2;
       upgrade*=3;
       myHeading.textContent = "머니머니머니:"+money;
-      upmoney.textContent = "고물점에서 산 도끼:"+upgrade+"원";
+      upmoney.textContent = "쿠팡에서 시킨 도끼:"+upgrade+"골드";
       moneyup+=1;
     }
-    else if(okclick==2)
+    else if(okclick===2)
     {
+      currentIndex=6;
       money-=upgrade;
       okclick=3;
       upgrade*=3;
       myHeading.textContent = "머니머니머니:"+money;
-      upmoney.textContent = "쿠팡에서 시킨 도끼:"+upgrade+"원";
+      upmoney.textContent = "일반 도끼:"+upgrade+"골드";
       moneyup+=2;
     }
-     else if(okclick==3)
+    else if(okclick===3)
     {
+      currentIndex=8;
       money-=upgrade;
       okclick=4;
       upgrade*=3;
       myHeading.textContent = "머니머니머니:"+money;
-      upmoney.textContent = "일반 도끼:"+upgrade+"원";
+      upmoney.textContent = "은도끼:"+upgrade+"골드";
       moneyup+=5;
+    }
+    else if(okclick===4)
+    {
+      currentIndex=10;
+      money-=upgrade;
+      okclick=5;
+      upgrade*=3;
+      myHeading.textContent = "머니머니머니:"+money;
+      upmoney.textContent = "금도끼:"+upgrade+"골드";
+      moneyup+=10;
+    }
+    else if(okclick===5)
+    {
+      currentIndex=12;
+      money-=upgrade;
+      okclick=6;
+      upgrade*=3;
+      myHeading.textContent = "머니머니머니:"+money;
+      upmoney.textContent = "비브라늄 도끼:"+upgrade+"골드";
+      moneyup+=20;
+    }
+    else if(okclick===6)
+    {
+      currentIndex=14;
+      money-=upgrade;
+      okclick=7;
+      upgrade*=3;
+      myHeading.textContent = "머니머니머니:"+money;
+      upmoney.textContent = "네더라이트 도끼:"+upgrade+"골드";
+      moneyup+=50;
+    }
+    else if(okclick===7)
+    {
+      currentIndex=16;
+      money-=upgrade;
+      okclick=8;
+      upgrade*=3;
+      myHeading.textContent = "머니머니머니:"+money;
+      upmoney.textContent = "묠니르:"+upgrade+"골드";
+      moneyup+=100;
+    }
+    else if(okclick===8)
+    {
+      okclick=9;
+      moneyup+=500;
+      currentIndex=18;
+      myHeading.textContent = "머니머니머니:"+money;
+      upmoney.textContent = "도끼 강화 완료";
     }
   }
 }
 document.addEventListener('keydown', () => {
   myHeading.textContent = "머니머니머니:"+money;
+  if(okclick==0)
+  {
     currentIndex++;
-    if (currentIndex >= slides.length) {
-      currentIndex = 0;
+    if(currentIndex===2)
+    {
+      currentIndex=0;
     }
-    else{
-      audio.play();
-      money+=moneyup;
+  }
+  else if(okclick==1)
+  {
+    currentIndex++;
+    if(currentIndex===4)
+    {
+      currentIndex=2;
     }
-    moveSlide();
+  }
+  else if(okclick===2)
+  {
+        currentIndex++;
+    if(currentIndex===6)
+    {
+      currentIndex=4;
+    }
+  }
+  else if(okclick===3)
+  {
+    currentIndex++;
+    if(currentIndex===8)
+    {
+      currentIndex=6;
+    }
+  }
+  else if(okclick===4)
+  {
+    currentIndex++;
+    if(currentIndex===10)
+    {
+      currentIndex=8;
+    }
+  }
+  else if(okclick===5)
+  {
+    currentIndex++;
+    if(currentIndex===12)
+    {
+      currentIndex=10;
+    }
+  }
+  else if(okclick===6)
+  {
+    currentIndex++;
+    if(currentIndex===14)
+    {
+      currentIndex=12;
+    }
+  }
+  else if(okclick===7)
+  {
+    currentIndex++;
+    if(currentIndex===16)
+    {
+      currentIndex=14;
+    }
+  }
+  else if(okclick===8)
+  {
+    currentIndex++;
+    if(currentIndex===18)
+    {
+      currentIndex=16;
+    }
+  }
+    else if(okclick===9)
+  {
+    currentIndex++;
+    if(currentIndex===20)
+    {
+      currentIndex=18;
+    }
+  }
+  audio.play();
+  money+=moneyup;
+  moveSlide();
 });
