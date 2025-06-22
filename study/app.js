72fd2dea60be33958b096ad2170d8c4d953b75e4
rhtnddd <qlalfql123@gmail.com>
2025-06-22 23:18:29 +0900

    html과제

--- study/app.js
+++ study/app.js
@@ -26,15 +26,34 @@ renderProducts(products);
 
 let click=0;
 let click1=0;
+//검색한거 삭제
+// document.getElementById('search-btn').addEventListener('click', function(){
+//     const keyword = document.getElementById('search-input').value.trim()
+
+//     const filtered = products.filter(item => item.title!==(keyword))
+
+//     renderProducts(filtered)
+// })
 document.getElementById('search-btn').addEventListener('click', function(){
-    const keyword = document.getElementById('search-input').value.trim()
-
-    const filtered = products.filter(item => item.title!==(keyword))
-
-    renderProducts(filtered)
+  const keyword = document.getElementById('search-input').value.trim()
+  if(keyword==='')
+  {
+    alert('삼품명을 적어주세요.');
+    return 0;
+  }
+  else if(products.some(item => item.title.includes(keyword)))
+  {
+    const filtered = products.filter(item => item.title.includes(keyword));
+    renderProducts(filtered);
+  }
+  else
+  {
+    alert('상품이 존재하지 않습니다.');
+    return 0;
+  }
 })
 
-
+//가격순 정렬
 document.getElementById('sort-price').addEventListener('click', function () {
   click++;
   if(click===2)
@@ -53,12 +72,12 @@ document.getElementById('sort-name').addEventListener('click', function () {
   click1++;
   if(click1===2)
   {
-      products.sort((a, b) => a.title >= b.title);
+      products.sort((a, b) => a.title.localeCompare(b.title));
       renderProducts(products);
       click1=0;
   }
   else{
-      products.sort((a, b) => b.title >= a.title);
+      products.sort((a, b) => b.title.localeCompare(a.title));
       renderProducts(products);
   }
 });
@@ -69,24 +88,37 @@ document.getElementById('add-product').addEventListener('click', () => {
     const priceInput = document.getElementById('new-price');
 
     const newTitle = titleInput.value.trim();
-    const newPrice = parseInt(priceInput.value);
+    const newPrice = priceInput.value.trim();
     // 배열에 추가
     if(newTitle === '')
     {
       alert('상품명을 입력해주세요.');
+      return 0;
     }
-    else if(newPrice === '')
+    if(newPrice === '')
     {
       alert('가격을 입력해주세요.');
+      return 0;
     }
     else
     {
       console.log('if문 통과');
-      const newTitle = titleInput.value.trim();
-      const newPrice = parseInt(priceInput.value);
       products.push({ title: newTitle, price: newPrice });
       renderProducts(products);
       titleInput.value = '';
       priceInput.value = '';
     }
+});
+const input = document.getElementById('new-price');
+    input.addEventListener('input', function () {
+    this.value = this.value.replace(/[^0-9.]/g, '');
+});
+input.addEventListener('keydown', function (e) {
+  const key = e.key;
+  const allowedKeys = ['Backspace', 'Delete', 'Tab', 'ArrowLeft', 'ArrowRight'];
+  const isNumber = /^[0-9]$/.test(key);
+  const isDot = key === '.';
+  if (!isNumber && !allowedKeys.includes(key) && !isDot) {
+    e.preventDefault();
+  }
 });
